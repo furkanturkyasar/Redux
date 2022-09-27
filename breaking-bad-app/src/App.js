4a1ee17b6b605e69848d8e9b04d5b62152a66235
Furkan Turkyasar <turkyasarfurkan@gmail.com>
2022-09-27 20:42:48 +0300

    react-router v6.4 studies

--- breaking-bad-app/src/App.js
+++ breaking-bad-app/src/App.js
@@ -1,7 +1,24 @@
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import Contact from "./components/Contacts";
+import Main from "./components/Main";
+
 import "./App.css";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Contact />}>
+      <Route index element={<Main />} />
+    </Route>
+  )
+);
+
 function App() {
-  return <div className="App"></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
